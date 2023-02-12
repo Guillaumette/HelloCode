19972ed193d3b803ffe06a1fb78d5bfcb8fb2bdb
Guillaumette <anna_kniazeva@bk.ru>
2023-02-12 15:25:26 +0300

    task about printing second number in 3-numbers number

---
+++ Homework_Seminar_2/Task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+marker:
+    Console.Write("Введите трехзначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number / 100 < 1)
+    {
+        Console.WriteLine("Число должно быть трехзначным");
+        goto marker;
+    }
+
+    int secondNum = number / 10 % 10;
+    Console.WriteLine(secondNum);
+
+
+
