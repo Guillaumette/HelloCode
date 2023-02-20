507e76c339ed1e4d964fd6ed2c2b47c5cd4b5a53
Guillaumette <anna_kniazeva@bk.ru>
2023-02-20 20:37:54 +0300

    finding summ all digits in number

---
+++ Homework_Seminar_4/Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int FindSum(int n)
+{
+    int res = 0;
+    while (n > 0)
+    {
+        res += n % 10;
+        n /= 10;
+    }
+    return res;
+}
+
+int number = ReadInt("Введите число: ");
+System.Console.WriteLine($"Сумма всех чисел в {number} равна {FindSum(number)}");
