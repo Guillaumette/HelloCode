65451aa55b64c8a2819640581d12544b853fd287
Guillaumette <anna_kniazeva@bk.ru>
2023-02-20 20:41:25 +0300

    filling array of 8 elements randomly

---
+++ Homework_Seminar_4/Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void FillArray(int[] array)
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(0, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int[] arr = new int[8];
+FillArray(arr);
+PrintArray(arr);
