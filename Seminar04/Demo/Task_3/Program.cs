3d28f03c686716e75bd191f1ac842fb66dacf51d
Guillaumette <anna_kniazeva@bk.ru>
2023-02-20 19:46:38 +0300

    finding factorial with function

---
+++ Seminar04/Demo/Task_3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Product(int n)
+{   
+    int res = 1;
+    if (n < 0)
+    {
+        System.Console.WriteLine("Нельзя посчитать факториал отрицательного числа");
+    }
+    else if (n == 0)
+    {
+        res = 1;
+    }
+    else
+    {
+        for (int i = 2; i <= n; i++)
+        {
+            res *= i;
+        }
+    }
+    return res;
+}
+
+int number = ReadInt("Введите число: ");
+System.Console.WriteLine($"Произведением чисел от 1 до {number} является значение {Product(number)}");
+
+number = ReadInt("Введите число: ");
+System.Console.WriteLine($"Произведением чисел от 1 до {number} является значение {Product(number)}");
