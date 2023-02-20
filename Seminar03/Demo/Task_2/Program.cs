461ad55f9710a4079bcf26fb2f91e57ad72b2556
Guillaumette <anna_kniazeva@bk.ru>
2023-02-20 14:57:28 +0300

    coordinates task

---
+++ Seminar03/Demo/Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает координаты точки X и Y, причем они оба не равны нулю,
+// и выдает номер четверти плоскости, в которой лежит эта точка.
+
+Console.Write("Введите значение X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0) System.Console.WriteLine("1 четверть");
+else if (x < 0 && y > 0) System.Console.WriteLine("2 четверть");
+else if (x < 0 && y < 0) System.Console.WriteLine("3 четверть");
+else if (x > 0 && y < 0) System.Console.WriteLine("4 четверть");
+else System.Console.WriteLine("Координата лежит на осях");
+    
+
