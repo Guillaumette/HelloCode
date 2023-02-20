9ad3858b723f58274e43a9976d71b41320baaedd
Guillaumette <anna_kniazeva@bk.ru>
2023-02-20 15:19:19 +0300

    resolution by array

--- Seminar03/Task_1/Program.cs
+++ Seminar03/Task_1/Program.cs
@@ -2,9 +2,10 @@
 
 System.Console.Write("Введите значение четверти: ");
 int quart = Convert.ToInt32(Console.ReadLine());
+string[] strArray = {"x > 0, y > 0", "x < 0, y > 0", "x < 0, y < 0", "x > 0, y < 0", "Введено некорректное значение четверти"};
 
-if (quart == 1) System.Console.WriteLine("x > 0, y > 0");
-else if (quart == 2) System.Console.WriteLine("x < 0, y > 0");
-else if (quart == 3) System.Console.WriteLine("x < 0, y < 0");
-else if (quart == 4) System.Console.WriteLine("x > 0, y < 0");
-else System.Console.WriteLine("Введено некорректное значение четверти");
+if (quart == 1) System.Console.WriteLine(strArray[0]);
+else if (quart == 2) System.Console.WriteLine(strArray[1]);
+else if (quart == 3) System.Console.WriteLine(strArray[2]);
+else if (quart == 4) System.Console.WriteLine(strArray[3]);
+else System.Console.WriteLine(strArray[4]);
