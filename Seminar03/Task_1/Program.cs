0e1324aa9c717f4b63c29ba4924815a44998ce59
Guillaumette <anna_kniazeva@bk.ru>
2023-02-20 15:23:20 +0300

    short resolution

--- Seminar03/Task_1/Program.cs
+++ Seminar03/Task_1/Program.cs
@@ -4,8 +4,11 @@ System.Console.Write("Введите значение четверти: ");
 int quart = Convert.ToInt32(Console.ReadLine());
 string[] strArray = {"x > 0, y > 0", "x < 0, y > 0", "x < 0, y < 0", "x > 0, y < 0", "Введено некорректное значение четверти"};
 
-if (quart == 1) System.Console.WriteLine(strArray[0]);
-else if (quart == 2) System.Console.WriteLine(strArray[1]);
-else if (quart == 3) System.Console.WriteLine(strArray[2]);
-else if (quart == 4) System.Console.WriteLine(strArray[3]);
-else System.Console.WriteLine(strArray[4]);
+if (quart > 0 && quart < 5)
+{
+    System.Console.WriteLine(strArray[quart - 1]);
+}
+else
+{
+    System.Console.WriteLine("Введено некорректное значение");
+}
