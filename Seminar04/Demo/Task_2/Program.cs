51b055eae3d81789262cf51533605e62e1a60448
Guillaumette <anna_kniazeva@bk.ru>
2023-02-20 19:23:24 +0300

    quantity of digits in number

---
+++ Seminar04/Demo/Task_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая на вход получает число и выдает количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+int NumLength(int n)
+{
+    int count = 0;
+
+    while (n > 0)
+    {
+        n /= 10;
+        count++;
+    }
+    
+    return count;
+}
+
+int number = ReadInt("Введите число: ");
+System.Console.WriteLine($"Количество цифр в числе {number}: {NumLength(number)}");
+
+number = ReadInt("Введите число: ");
+System.Console.WriteLine($"Количество цифр в числе {number}: {NumLength(number)}");
+
+number = ReadInt("Введите число: ");
+System.Console.WriteLine($"Количество цифр в числе {number}: {NumLength(number)}");
