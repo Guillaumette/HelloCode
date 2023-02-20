b96b80a1702c2ea913ef792fa5e37b53def06a50
Guillaumette <anna_kniazeva@bk.ru>
2023-02-20 15:03:15 +0300

    coordinates by array

--- Seminar03/Demo/Task_2/Program.cs
+++ Seminar03/Demo/Task_2/Program.cs
@@ -1,15 +1,16 @@
 ﻿// Напишите программу, которая на вход принимает координаты точки X и Y, причем они оба не равны нулю,
 // и выдает номер четверти плоскости, в которой лежит эта точка.
 
+// Вводим массив и выделяем ему две ячеки памяти, т.к. нам надо всего две координаты
+int[] coords = new int[2];
 Console.Write("Введите значение X: ");
-int x = Convert.ToInt32(Console.ReadLine());
+coords[0] = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите значение Y: ");
-int y = Convert.ToInt32(Console.ReadLine());
+coords[1] = Convert.ToInt32(Console.ReadLine());
 
-if (x > 0 && y > 0) System.Console.WriteLine("1 четверть");
-else if (x < 0 && y > 0) System.Console.WriteLine("2 четверть");
-else if (x < 0 && y < 0) System.Console.WriteLine("3 четверть");
-else if (x > 0 && y < 0) System.Console.WriteLine("4 четверть");
+if (coords[0] > 0 && coords[1] > 0) System.Console.WriteLine("1 четверть");
+else if (coords[0] < 0 && coords[1] > 0) System.Console.WriteLine("2 четверть");
+else if (coords[0] < 0 && coords[1] < 0) System.Console.WriteLine("3 четверть");
+else if (coords[0] > 0 && coords[1] < 0) System.Console.WriteLine("4 четверть");
 else System.Console.WriteLine("Координата лежит на осях");
-    
-
+   
