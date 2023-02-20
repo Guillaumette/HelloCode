428e58e85c960ec27ad52db378df01eab95b51f3
Guillaumette <anna_kniazeva@bk.ru>
2023-02-20 15:12:24 +0300

    task 1 for self work: find range of x and y values ​​when entering quarter value

---
+++ Seminar03/Task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая по заданному номеру четверти показывает диапазон возможных координат точек x и y в этой четверти.
+
+System.Console.Write("Введите значение четверти: ");
+int quart = Convert.ToInt32(Console.ReadLine());
+
+if (quart == 1) System.Console.WriteLine("x > 0, y > 0");
+else if (quart == 2) System.Console.WriteLine("x < 0, y > 0");
+else if (quart == 3) System.Console.WriteLine("x < 0, y < 0");
+else if (quart == 4) System.Console.WriteLine("x > 0, y < 0");
+else System.Console.WriteLine("Введено некорректное значение четверти");
