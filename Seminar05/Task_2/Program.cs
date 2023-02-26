0fba74117dd8ee869bde1bd8c7e755af8438806f
Guillaumette <anna_kniazeva@bk.ru>
2023-02-26 18:53:05 +0300

    chancging positive elements to negative vice versa

---
+++ Seminar05/Task_2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу замены элементов массива: положительные элементы замените на отрицательные и наоборот.
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+void ChangeArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = -array[i];
+    }
+}
+
+const int SIZE = 4;
+const int LEFT_RANGE = -9;
+const int RIGHT_RANGE = 9;
+
+var arr = GenerateArray(SIZE, LEFT_RANGE, RIGHT_RANGE);
+PrintArray(arr);
+ChangeArray(arr);
+PrintArray(arr);
