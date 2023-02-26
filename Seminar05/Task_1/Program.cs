a9f8506fd08aacf701408f7475458855a17b826e
Guillaumette <anna_kniazeva@bk.ru>
2023-02-26 17:42:18 +0300

    out int variables, methods, finding sum positive and sum negative elements of array

---
+++ Seminar05/Task_1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
+// Найдите сумму отрицательных и положительных элементов массива.
+
+int[] GenerateArray(int size)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(-9, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+void CountSum(int[] array, out int sumPositive, out int sumNegative)
+{
+    sumPositive = 0;
+    sumNegative = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            sumPositive += array[i];
+        }
+        else
+        {
+            sumNegative += array[i];
+        }
+    }
+}
+var arr = GenerateArray(12);
+
+PrintArray(arr);
+CountSum(arr, out int sumPos, out int sumNeg);
+
+System.Console.WriteLine($"Сумма положительных чисел: {sumPos}");
+System.Console.WriteLine($"Сумма отрицательных чисел: {sumNeg}");
