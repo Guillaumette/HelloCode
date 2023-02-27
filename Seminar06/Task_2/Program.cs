5f2b6bf50ddd466ae22b7fc89334b618761274e6
Guillaumette <anna_kniazeva@bk.ru>
2023-02-27 15:41:43 +0300

    checking if triangle without array

---
+++ Seminar06/Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, можнт ли существовать треугольник со сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона меньше суммы двух других сторон.
+
+void IfTriangle(int num1, int num2, int num3)
+{
+    if (num1 + num2 > num3 && num1 + num3 > num2 && num2 + num3 > num1)
+    {
+        System.Console.WriteLine($"Треугольник со сторонами, равными {num1}, {num2}, {num3} может существовать");
+    }
+    else
+    {
+        System.Console.WriteLine($"Треугольник со сторонами, равными {num1}, {num2}, {num3} не может существовать");
+    }
+}
+
+System.Console.Write("Введите значение первой стороны: ");
+int side1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите значение второй стороны: ");
+int side2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите значение третьей стороны: ");
+int side3 = Convert.ToInt32(Console.ReadLine());
+IfTriangle(side1, side2, side3);
