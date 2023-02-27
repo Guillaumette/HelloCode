e7099b6bf1d5032f5f2c08e871c0adc5f1380c5b
Guillaumette <anna_kniazeva@bk.ru>
2023-02-27 17:47:25 +0300

    fibonacci without recursion

---
+++ Seminar06/Task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: 0 и 1.
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Fibonacci(int num)
+{
+    System.Console.Write("0 1 ");
+    int result = 0;
+    int a = 0, b = 1;
+    for (int i = 2; i < num; i++)
+    {
+        result = a + b;
+        a = b;
+        b = result;
+        System.Console.Write(result + " ");
+    }
+}
+
+int N = ReadInt("Введите число: ");
+Fibonacci(N);
