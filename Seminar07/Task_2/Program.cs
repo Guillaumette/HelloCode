df44a29d60e731f540583aeccdbfcdd85dffd3ff
Guillaumette <anna_kniazeva@bk.ru>
2023-03-05 16:27:16 +0300

    filling matrix by formula A = m + n, where m and n are indexes of each element by row and column

---
+++ Seminar07/Task_2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте двумерный массив размерностью m x n, каждый элемент которого находится по формуле: А = m + n
+// rows = 3; cols = 4
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int x, int y)
+{
+    int[,] matrix = new int[x, y];
+    for(int i = 0; i < x; i++)
+    {
+        for(int j = 0; j < y; j++)
+        {
+            matrix[i,j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++) // 0 - итерация по столбцам
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++) // 1 - итерации по строкам
+        {
+            System.Console.Write(matrix[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int m = ReadInt("Введите количество строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+var myMatrix = GenerateMatrix(m, n);
+PrintMatrix(myMatrix);
