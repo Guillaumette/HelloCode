00dd036d0b2338f8d1b3673625f03426c737e3cd
Guillaumette <anna_kniazeva@bk.ru>
2023-03-05 22:15:42 +0300

    square elements with even indexes in matrix

---
+++ Seminar07/Task_4/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса четные, и замените их на их квадраты.
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int x, int y)
+{
+    int[,] matrix = new int[x, y];
+    Random rand = new Random();
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            matrix[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) // 0 - итерация по столбцам
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // 1 - итерации по строкам
+        {
+            System.Console.Write(matrix[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void SquareEvens(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2) // 0 - итерация по столбцам
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+            matrix[i, j] *= matrix[i, j];
+            System.Console.Write(matrix[i, j] + " ");
+        }
+    }
+    System.Console.WriteLine();
+}
+
+
+int m = ReadInt("Введите количество строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+var myMatrix = GenerateMatrix(m, n);
+PrintMatrix(myMatrix);
+System.Console.WriteLine();
+SquareEvens(myMatrix);
