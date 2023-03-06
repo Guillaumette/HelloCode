091b3d5bc1c058723fdb9b186290dff9fd1996d9
Guillaumette <anna_kniazeva@bk.ru>
2023-03-06 22:27:48 +0300

    average of each matrix column

---
+++ Homework_Seminar_7/Task_3/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int x, int y)
+{
+    int[,] matrix = new int[x, y];
+    Random rand = new Random();
+    for(int i = 0; i < x; i++)
+    {
+        for(int j = 0; j < y; j++)
+        {
+            matrix[i,j] = rand.Next(0,10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++) // 0 - итерация по строкам
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++) // 1 - итерации по столбцам
+        {
+            System.Console.Write(matrix[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[] FindAverage(int[,] matrix)
+{
+    double[] sum = new double[matrix.GetLength(1)];
+    double[] average = new double[matrix.GetLength(1)];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum[j] += matrix[i,j];
+            sum[j] = Math.Round(sum[j], 1);
+            average[j] = sum[j] / matrix.GetLength(0);
+        }
+    }
+    return average;
+}
+
+
+int m = ReadInt("Введите количество строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+var myMatrix = GenerateMatrix(m, n);
+PrintMatrix(myMatrix);
+System.Console.WriteLine($"Среднее арифметическое каждого столбца равно {string.Join("; " , FindAverage(myMatrix))}");
