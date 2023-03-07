200fcc6112b5283a52a4add6fb6e3486da4bcd02
Guillaumette <anna_kniazeva@bk.ru>
2023-03-07 15:38:00 +0300

    changing strings and columns of matrix

---
+++ Seminar08/Task_2/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задате двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно,
+// программа должна вывести сообщение для пользователя.
+// (Не учитываем, когда это невозможно, делаем и для разной размерности массива тоже).
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int x, int y)
+{
+    int[,] matrix = new int[x, y];
+    Random rand = new Random();
+    for(int i = 0; i < x; i++)
+    {
+        for(int j = 0; j < y; j++)
+        {
+            matrix[i,j] = rand.Next(0,10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++) // 0 - итерация по строкам
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++) // 1 - итерации по столбцам
+        {
+            System.Console.Write(matrix[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] TurnMatrix(int[,] matrix)
+{
+    int[,] matrixTurn = new int[matrix.GetLength(1), matrix.GetLength(0)]; // так как мы поворачиваем матрицу, размерность обратная
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrixTurn[j,i] = matrix[i,j]; // наоборот, так как иначе будет проблема выхода из массиво
+        }
+    }
+    return matrixTurn;
+}
+
+
+int m = ReadInt("Введите количество строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+var myMatrix = GenerateMatrix(m, n);
+PrintMatrix(myMatrix);
+System.Console.WriteLine();
+PrintMatrix(TurnMatrix(myMatrix));
