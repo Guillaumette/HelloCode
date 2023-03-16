280122fd2d877657eb7f7c073fb4c67cb8b2c92b
Guillaumette <anna_kniazeva@bk.ru>
2023-03-16 15:49:48 +0300

    power with recursion

---
+++ Seminar09/Task_4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B и возводит A в степень B.
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int RecursionPower(int m, int n)
+{
+    if (n == 0) return 1;
+    return m * RecursionPower(m, n - 1);
+}
+
+int a = ReadInt("Введите чисто А: ");
+int b = ReadInt("Введите число В: ");
+System.Console.WriteLine(RecursionPower(a, b));
