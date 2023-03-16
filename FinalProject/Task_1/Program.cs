37811c2b6e1be59e2e0478e421106fcee2c5f97c
Guillaumette <anna_kniazeva@bk.ru>
2023-03-16 14:38:42 +0300

    numbers from N to 1 with recursion

---
+++ FinalProject/Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void WriteNumbers(int number)
+{
+    if (number == 0)
+    {
+        return;
+    }
+    System.Console.Write(number + " ");
+    WriteNumbers(number - 1);
+}
+
+
+WriteNumbers(ReadInt("Введите число: "));
