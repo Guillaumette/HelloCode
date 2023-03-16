f9e597f25d7d6742bf296f5e7a073cb904ae68bc
Guillaumette <anna_kniazeva@bk.ru>
2023-03-16 16:06:29 +0300

    resolution with recursion in both functions

--- FinalProject/Task_2/Program.cs
+++ FinalProject/Task_2/Program.cs
@@ -14,18 +14,12 @@ void WriteNumbers(int m, int n)
 }
 
 int SumNumbers(int m, int n)
+
 {
-    int sum = 0;
-    int start = m;
-    int finish = n;
-    if (m > n)
-    {
-        start = n;
-        finish = m;
-    }
-    sum = (finish + start) * (finish - start + 1) / 2;
-    return sum;
+    if(n == 0) return 0;
+    return n + SumNumbers(m, n - 1);
 }
+
 
 int m = ReadInt("Введите число M: ");
 int n = ReadInt("Введите число N: ");
