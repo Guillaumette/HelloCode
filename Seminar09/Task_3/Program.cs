9b16abf5f25d036e9a40a14c75dcb04d8e52a1dc
Guillaumette <anna_kniazeva@bk.ru>
2023-03-16 15:40:32 +0300

    finding sum of ciffres in number with recursion

---
+++ Seminar09/Task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int FindSumCiffres(int number)
+{
+    if (number == 0) return 0;
+    return number % 10 + FindSumCiffres(number / 10);
+}
+
+int n = ReadInt("Введите число: ");
+System.Console.WriteLine($"Сумма цифр числа {n} равна {FindSumCiffres(n)}");
